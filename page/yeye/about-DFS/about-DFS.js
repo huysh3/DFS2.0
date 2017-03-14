cf9f8e1866d47aa4595cfb9b5aa6b442e176bb24
胡钰盛 <wilson.hu@shendun.info>
2017-03-15 02:54:16 +0800

    新增: 首页

--- page/yeye/about-DFS/about-DFS.js
+++ page/yeye/about-DFS/about-DFS.js
@@ -1,1 +1,21 @@
-Page({})
+var domain = 'https://15580083.qcloud.la/'
+var qcloud = require('../../../vendor/qcloud-weapp-client-sdk/index');
+
+Page({
+  data: {
+    "lists": ''
+  },
+  onShow: function() {
+    var _this = this
+    // qcloud.request({
+    //   login: true,
+    //   url: domain + 'Home/weapp/class_list',
+    //   success(res) {
+    //     console.log(res.data)
+    //     _this.setData({lists: res.data})
+    //   },
+    //   fail(error) {
+    //   }
+    // })
+  }
+})
