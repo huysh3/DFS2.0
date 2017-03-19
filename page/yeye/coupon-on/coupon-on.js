f4a339f75ae47e74234d96e3f675f6ee48f48db2
胡钰盛 <wilson.hu@shendun.info>
2017-03-20 03:41:06 +0800

    dist

--- page/yeye/coupon-on/coupon-on.js
+++ page/yeye/coupon-on/coupon-on.js
@@ -1,1 +1,27 @@
-Page({})
+var domain = 'https://15580083.qcloud.la/'
+var qcloud = require('../../../vendor/qcloud-weapp-client-sdk/index');
+
+Page({
+  data: {
+    qrcodeState: false,
+    qrCodeSrc: '../../../image/QRCode1.png'
+  },
+  onShow: function() {
+    var _this = this
+  },
+  judgeQrcode: function(e) {
+    var _this = this
+    if (e.detail.value === '347892') {
+      wx.hideKeyboard()
+      _this.setData({
+        qrcodeState: true,
+        qrCodeSrc: '../../../image/QRCode2.png'
+      })
+    }
+    setTimeout(function() {
+      _this.setData({
+        qrCodeSrc: '../../../image/QRCode1.png'
+      })
+    }, 30000)
+  }
+})
