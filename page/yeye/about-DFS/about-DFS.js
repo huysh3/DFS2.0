7bef30f2d770538f16f69654bd14a8a6c699bc98
胡钰盛 <wilson.hu@shendun.info>
2017-03-19 21:45:34 +0800

    dev

--- page/yeye/about-DFS/about-DFS.js
+++ page/yeye/about-DFS/about-DFS.js
@@ -3,19 +3,23 @@ var qcloud = require('../../../vendor/qcloud-weapp-client-sdk/index');
 
 Page({
   data: {
-    "lists": ''
+    "lists": '',
+    coupon_status: '',
   },
   onShow: function() {
     var _this = this
-    // qcloud.request({
-    //   login: true,
-    //   url: domain + 'Home/weapp/class_list',
-    //   success(res) {
-    //     console.log(res.data)
-    //     _this.setData({lists: res.data})
-    //   },
-    //   fail(error) {
-    //   }
-    // })
+
+    qcloud.request({
+      login: true,
+      url: domain + 'Home/coupon/coupon_status',
+      success(res) {
+        console.log(res.data)
+        _this.setData({
+          coupon_status: res.data.coupon_status
+        })
+      },
+      fail(error) {
+      }
+    })
   }
 })
