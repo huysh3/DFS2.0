2178382543413fedaff3e4744932ee35376fde55
胡钰盛 <wilson.hu@shendun.info>
2017-03-25 01:07:45 +0800

    fix - promotions页面报错

--- page/yeye/promotions/promotions.js
+++ page/yeye/promotions/promotions.js
@@ -1,3 +1,29 @@
+var domain = 'https://15580083.qcloud.la/'
+var qcloud = require('../../../vendor/qcloud-weapp-client-sdk/index');
+
+// 显示繁忙提示
+var showBusy = text => wx.showToast({
+    title: text,
+    icon: 'loading',
+    duration: 10000
+});
+
+// 显示成功提示
+var showSuccess = text => wx.showToast({
+    title: text,
+    icon: 'success'
+});
+
+// 显示失败提示
+var showModel = (title, content) => {
+    wx.hideToast();
+    wx.showModal({
+        title,
+        content: JSON.stringify(content),
+        showCancel: false
+    });
+};
+
 Page({
   data: {
     coupon_status: ''
