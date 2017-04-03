5452f80de12bc270b03453f4d0da14f94efddcd3
胡钰盛 <wilson.hu@shendun.info>
2017-04-04 02:57:35 +0800

    dist - 调整promotions页面图片到自适应

--- page/DFS/promotions/promotions.js
+++ page/DFS/promotions/promotions.js
@@ -26,53 +26,7 @@ var showModel = (title, content) => {
 
 Page({
   data: {
-    promotion_list: [],
-    img_list: [
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions1.jpg",
-        height: "577rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions2.jpg",
-        height: "440rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions3.jpg",
-        height: "443rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions4.jpg",
-        height: "634rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions5.jpg",
-        height: "240rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions6.jpg",
-        height: "590rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions7.jpg",
-        height: "357rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions8.jpg",
-        height: "319rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions9.jpg",
-        height: "292rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions10.jpg",
-        height: "282rpx"
-      },
-      {
-        url: "https://om536p71r.qnssl.com/In_Store_Promotions10.jpg",
-        height: "299rpx"
-      }
-    ]
+    promotion_list: []
   },
   onLoad() {
     var _this = this
