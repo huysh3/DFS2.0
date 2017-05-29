2a653229adea3adc9dc8cf8df21e8eec48ac623e
胡钰盛 <wilson.hu@shendun.info>
2017-05-29 18:55:27 +0800

    dev

--- page/DFS/services/services.js
+++ page/DFS/services/services.js
@@ -4,7 +4,7 @@ Page({
   },
   phoneCall: function() {
     wx.makePhoneCall({
-      phoneNumber: '13602455397' //仅为示例，并非真实的电话号码
+      phoneNumber: '18819253238' //仅为示例，并非真实的电话号码
     })
   },
   routerGoHome: function() {
